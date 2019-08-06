b2d1b85951a1ba929a3c2381f326f271127e0ab1
khj0209 <gaegom0209@gmail.com>
2019-08-06 17:22:53 +0900

    Update packet_func.h

--- packet_func.h
+++ packet_func.h
@@ -4,7 +4,7 @@ void myInfo(char* itf,u_char *mac,uint8_t *ip);
 void atoiArgv(char *ip,uint8_t* rip);
 void normalArp(uint8_t* mIp,u_char* mMac,uint8_t* senIp,char* dev);
 int checkL3type(const u_char* pack);
-void listenArp(char* argv,uint8_t* senIp, u_char* senMac);
+void listenArp(uint8_t* senIp, u_char* senMac,char* argv);
 void sendArp(struct eth_header* eth, struct arp_header* arp,char* dev);
 void makeArp(uint8_t* mIp,u_char* sMac,uint8_t* senIp, struct eth_header* eth,struct arp_header* arp);
 void makeAttackArp(u_char* mMac,u_char* senMac,uint8_t* tarIp,uint8_t* senIp,struct eth_header* eth,struct arp_header* arp);
